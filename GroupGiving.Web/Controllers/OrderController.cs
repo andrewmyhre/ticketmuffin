95842a8d6891ce247631c98eccf2a5489ffd4dae
Andrew Myhre <andrew.myhre@gmail.com>
2011-05-22 20:20:12 +0100

    azure deployment working

--- GroupGiving.Web/Controllers/OrderController.cs
+++ GroupGiving.Web/Controllers/OrderController.cs
@@ -14,7 +14,7 @@ namespace GroupGiving.Web.Controllers
         //
         // GET: /Order/
 
-        public ActionResult Index(int eventId)
+        public ActionResult Index(int? eventId)
         {
             return View();
         }
@@ -26,22 +26,7 @@ namespace GroupGiving.Web.Controllers
 
         public ActionResult StartRequest()
         {
-            IApiClient payPal = new ApiClient(new ApiClientSettings()
-            {
-                Username = "seller_1304843436_biz_api1.gmail.com",
-                Password = "1304843443",
-                Signature = "AFcWxV21C7fd0v3bYYYRCpSSRl31APG52hf-AmPfK7eyvf7LBc0.0sm7"
-            });
-            var request = new PayRequest();
-            request.ActionType = "PAY";
-            request.CurrencyCode = "USD";
-            request.FeesPayer = "EACHRECEIVER";
-            request.Memo = "test order";
-            request.CancelUrl = "http://" + Request.Url.Authority + "/Order/Cancel?payKey=${payKey}.";
-            request.ReturnUrl = "http://" + Request.Url.Authority + "/Order/Return?payKey=${payKey}.";
-            request.Receivers.Add(new Receiver("10", "seller_1304843436_biz@gmail.com"));
-            request.Receivers.Add(new Receiver("9", "sellr2_1304843519_biz@gmail.com"));
-            var response = payPal.SendPayRequest(request);
+            PayResponse response = SendPaymentRequest();
 
             var viewModel = new OrderRequestViewModel();
             viewModel.PayPalPostUrl = ConfigurationManager.AppSettings["PayFlowProPaymentPage"];
@@ -53,6 +38,26 @@ namespace GroupGiving.Web.Controllers
                 return View(viewModel);
 
             return Redirect(string.Format("https://www.sandbox.paypal.com/webscr?cmd=_ap-payment&paykey={0}", response.PayKey));
+        }
+
+        private PayResponse SendPaymentRequest()
+        {
+            IApiClient payPal = new ApiClient(new ApiClientSettings()
+                                                  {
+                                                      Username = "seller_1304843436_biz_api1.gmail.com",
+                                                      Password = "1304843443",
+                                                      Signature = "AFcWxV21C7fd0v3bYYYRCpSSRl31APG52hf-AmPfK7eyvf7LBc0.0sm7"
+                                                  });
+            var request = new PayRequest();
+            request.ActionType = "PAY";
+            request.CurrencyCode = "USD";
+            request.FeesPayer = "EACHRECEIVER";
+            request.Memo = "test order";
+            request.CancelUrl = "http://" + Request.Url.Authority + "/Order/Cancel?payKey=${payKey}.";
+            request.ReturnUrl = "http://" + Request.Url.Authority + "/Order/Return?payKey=${payKey}.";
+            request.Receivers.Add(new Receiver("10", "seller_1304843436_biz@gmail.com"));
+            request.Receivers.Add(new Receiver("9", "sellr2_1304843519_biz@gmail.com"));
+            return payPal.SendPayRequest(request);
         }
 
         public ActionResult Return()
