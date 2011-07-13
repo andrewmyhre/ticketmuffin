19c8e457543b85e06192d74f828e0ff2d78f6eb8
Andrew Myhre <andrew.myhre@gmail.com>
2011-07-13 21:35:07 +0100

    updated design for pledge page (purchase step 1)

---
+++ GroupGiving.Web/Scripts/pledge.js
@@ -0,0 +1,31 @@
+﻿var currencySymbol;
+$(document).ready(function () {
+    currencySymbol = $('#c').html();
+    updateTotals();
+    $('select[name=quantity]').change(function () { updateAttendeeList($('select[name=quantity]').val()); updateTotals(); });
+});
+
+function updateAttendeeList(quantity) {
+    while ($('#attendeeList').children().length > quantity) {
+        $('#attendeeList').children().last().remove();
+    }
+
+    while ($('#attendeeList').children().length < quantity) {
+        var att = $('#attendeeTemplate').clone();
+        att.show().appendTo('#attendeeList');
+        att.find('.attendeeNumber').html($('#attendeeList').children().length);
+        att.find('input[name=attendeeName]').watermark();
+    }
+    
+}
+
+function updateTotals() {
+    var total = $('input[name=ticketPrice]').val();
+    var quantity = $('select[name=quantity]').val();
+    $('.total').html(currencySymbol + (total * quantity).formatMoney(2, '.', ','));
+}
+
+Number.prototype.formatMoney = function (c, d, t) {
+    var n = this, c = isNaN(c = Math.abs(c)) ? 2 : c, d = d == undefined ? "," : d, t = t == undefined ? "." : t, s = n < 0 ? "-" : "", i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
+    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+};
