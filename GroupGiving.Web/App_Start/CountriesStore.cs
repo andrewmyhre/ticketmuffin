917adf56f8418df1bcb5757ea548381447fc8a54
andrew <andrew.myhre@gmail.com>
2011-10-08 22:59:43 +0100

    wrapped countries loading code in try/catch

--- GroupGiving.Web/App_Start/CountriesStore.cs
+++ GroupGiving.Web/App_Start/CountriesStore.cs
@@ -7,25 +7,33 @@ using System.Web;
 using System.Web.Hosting;
 using Com.StellmanGreene.CSVReader;
 using GroupGiving.Core.Services;
+using log4net;
 
 [assembly: WebActivator.PreApplicationStartMethod(typeof(GroupGiving.Web.App_Start.CountriesStore), "LoadCountries")]
 namespace GroupGiving.Web.App_Start
 {
     public class CountriesStore
     {
+        private static ILog _log = LogManager.GetLogger(typeof (CountriesStore));
         public static List<Country> Countries { get; set; } 
         public static void LoadCountries()
         {
-            Countries = new List<Country>();
-            using (var filestream= File.OpenRead(HostingEnvironment.MapPath("~/App_Data/countrylist.csv")))
-            using (var reader = new StreamReader(filestream))
+            try
             {
-                CSVReader csv = new CSVReader(reader);
-                var table = csv.CreateDataTable(true);
-                foreach(DataRow row in table.Rows)
+                Countries = new List<Country>();
+                using (var filestream = File.OpenRead(HostingEnvironment.MapPath("~/App_Data/countrylist.csv")))
+                using (var reader = new StreamReader(filestream))
                 {
-                    Countries.Add(new Country((string)row["Common Name"]));
+                    CSVReader csv = new CSVReader(reader);
+                    var table = csv.CreateDataTable(true);
+                    foreach (DataRow row in table.Rows)
+                    {
+                        Countries.Add(new Country((string) row["Common Name"]));
+                    }
                 }
+            } catch (Exception ex)
+            {
+                _log.Warn("Could not load countries data", ex);
             }
         }
     }
