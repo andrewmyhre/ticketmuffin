853eeb7c39765a372dc8a812c2cf250ad26a4031
andrew <andrew.myhre@gmail.com>
2011-12-23 19:42:32 +0000

    configuration service now deals with incomplete configuration

--- GroupGiving.Core/Services/SiteConfigurationService.cs
+++ GroupGiving.Core/Services/SiteConfigurationService.cs
@@ -24,19 +24,49 @@ namespace GroupGiving.Core.Services
             {
                 _log.Debug("loading site configuration");
                 var configuration = session.Query<SiteConfiguration>().FirstOrDefault();
-                if (configuration == null)
-                {
-                    _log.Debug("no stored configuration - creating");
-                    configuration = new SiteConfiguration();
-                    configuration.PayFlowProConfiguration = new PayFlowProConfiguration();
-                    configuration.AdaptiveAccountsConfiguration = new AdaptiveAccountsConfiguration();
-                    configuration.JustGivingApiConfiguration = new JustGivingApiConfiguration();
-                    configuration.DatabaseConfiguration = new DatabaseConfiguration();
-                    session.Store(configuration);
-                    session.SaveChanges();
-
-                }
+                EnsureValidConfigurationObject(configuration, session);
                 return configuration;
+            }
+        }
+
+        private void EnsureValidConfigurationObject(ISiteConfiguration configuration, IDocumentSession session)
+        {
+            bool storeNew = false, update=false;
+            if (configuration == null)
+            {
+                configuration = new SiteConfiguration();
+                storeNew = true;
+            }
+            if (configuration.PayFlowProConfiguration == null)
+            {
+                configuration.PayFlowProConfiguration = new PayFlowProConfiguration();
+                update = true;
+            }
+            if (configuration.AdaptiveAccountsConfiguration == null)
+            {
+                configuration.AdaptiveAccountsConfiguration = new AdaptiveAccountsConfiguration();
+                update = true;
+            }
+            if (configuration.JustGivingApiConfiguration == null)
+            {
+                configuration.JustGivingApiConfiguration = new JustGivingApiConfiguration();
+                update = true;
+            }
+            if (configuration.DatabaseConfiguration == null)
+            {
+                configuration.DatabaseConfiguration = new DatabaseConfiguration();
+                update = true;
+            }
+
+            if (storeNew)
+            {
+                session.Store(configuration);
+                session.SaveChanges();
+            }
+
+            if (update)
+            {
+                session.SaveChanges();
             }
         }
     }
