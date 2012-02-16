59aaa3d9331244ac96931dead064d9dcc0e79ffa
Tad <andrew.myhre@gmail.com>
2012-02-16 22:59:41 +0000

    added about pages

---
+++ GroupGiving.Web/Controllers/AboutController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroupGiving.Web.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /About/
+
+        public ActionResult TermsAndConditions()
+        {
+            return View();
+        }
+
+        public ActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
