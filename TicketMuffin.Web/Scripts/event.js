ea000a293bbf4a16da238a33ccc9f668109e4f6d
Andrew Myhre <andrew.myhre@gmail.com>
2012-10-06 12:48:49 +0100

    adds progress indicator and ui improvements to event contact form

--- TicketMuffin.Web/Scripts/event.js
+++ TicketMuffin.Web/Scripts/event.js
@@ -5,12 +5,18 @@
         $('a.contact-organiser').toggle();
         $('div.contact-box').slideToggle();
         $('div.contact-box textarea').focus();
+        $('form#contactForm input[type=submit]').removeAttr('disabled');
+        $('#message-sent').hide();
     });
 
     $('#contactForm').submit(function () {
         var message = $('div.contact-box textarea').val();
         var senderName = $('div.contact-box input[name=senderName]').val();
         var senderEmail = $('div.contact-box input[name=senderEmail]').val();
+
+        $('#contactProgress').show();
+        
+        $('form#contactForm input[type=submit]').attr('disabled', 'true');
 
         $.post($('#contactForm').attr('action'),
             'message=' + message + '&senderName=' + senderName + '&senderEmail=' + senderEmail,
@@ -27,5 +33,6 @@
         $('div.contact-box input[name=senderName]').val('');
         $('div.contact-box input[name=senderEmail]').val('');
         $('#message-sent').show();
+        $('#contactProgress').hide();
     }
 });
